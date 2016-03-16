a5a8e0a26b2c819c1f9db9693d62252777c0663a
yuronfu <C14011073@mail.ncku.edu.tw>
2016-03-16 20:57:28 +0800

    fix some problem in these files

--- test-bubble.sh
+++ test-bubble.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo 'b main' >> _tmp.gdb
-echo 'r' >> _tmp.gdb
-echo 'source scripts/create_list.gdb' >> _tmp.gdb
-echo 'source scripts/print_list.gdb' >> _tmp.gdb
-echo 'source scripts/free_list.gdb' >> _tmp.gdb
+echo 'b main' >> bubble_tmp.gdb
+echo 'r' >>  bubble_tmp.gdb
+echo 'source scripts/create_list.gdb' >> bubble_tmp.gdb
+echo 'source scripts/print_list.gdb' >>  bubble_tmp.gdb
+echo 'source scripts/free_list.gdb' >>  bubble_tmp.gdb
 
-echo 'set $head = (List**)malloc(sizeof(List*))' >> _tmp.gdb
+echo 'set $head = (List**)malloc(sizeof(List*))' >>  bubble_tmp.gdb
 
-echo 'set logging file '$2>>_tmp.gdb
+echo 'set logging file '$2>> bubble_tmp.gdb
 
 echo ' '>>$2
 exec < $1
@@ -18,35 +18,35 @@ do
 	read var
 	list_length=$var
 
-	echo 'create_list (*($head)) '$list_length >> _tmp.gdb
+	echo 'create_list (*($head)) '$list_length >>  bubble_tmp.gdb
 	read var
 	list_value=($var)
 
-	echo 'set $_head = (*($head))' >> _tmp.gdb
+	echo 'set $_head = (*($head))' >>  bubble_tmp.gdb
 
 	for value in "${list_value[@]}"
 	do
-		echo 'p $_head->value = '$value >> _tmp.gdb
-		echo 'p $_head = $_head->next' >> _tmp.gdb
+		echo 'p $_head->value = '$value >>  bubble_tmp.gdb
+		echo 'p $_head = $_head->next' >>  bubble_tmp.gdb
 	done
 
-	echo 'set logging on' >> _tmp.gdb
-	echo 'p "test begin"' >> _tmp.gdb
-	echo 'p "old_list"' >> _tmp.gdb
-	echo 'printf_list (*($head))' >> _tmp.gdb
-	echo 'set logging off' >> _tmp.gdb
-	echo 'p bubble_sort($head)' >> _tmp.gdb
-	echo 'set logging on' >> _tmp.gdb
-	echo 'p "new_list"' >> _tmp.gdb
-	echo 'printf_list (*($head))' >> _tmp.gdb
+	echo 'set logging on' >>  bubble_tmp.gdb
+	echo 'p "test begin"' >>  bubble_tmp.gdb
+	echo 'p "old_list"' >>  bubble_tmp.gdb
+	echo 'printf_list (*($head))' >>  bubble_tmp.gdb
+	echo 'set logging off' >>  bubble_tmp.gdb
+	echo 'p bubble_sort($head)' >>  bubble_tmp.gdb
+	echo 'set logging on' >>  bubble_tmp.gdb
+	echo 'p "new_list"' >>  bubble_tmp.gdb
+	echo 'printf_list (*($head))' >>  bubble_tmp.gdb
 
-	echo 'set logging off' >> _tmp.gdb
-	echo 'free_list (*($head))' >> _tmp.gdb
+	echo 'set logging off' >>  bubble_tmp.gdb
+	echo 'free_list (*($head))' >>  bubble_tmp.gdb
 done
 
-echo 'q' >> _tmp.gdb
-echo 'y' >> _tmp.gdb
+echo 'q' >>  bubble_tmp.gdb
+echo 'y' >>  bubble_tmp.gdb
 
-gdb -q -x _tmp.gdb bin-bubble >>/dev/null
+gdb -q -x  bubble_tmp.gdb bin-bubble >>/dev/null
 
-rm _tmp.gdb
+rm  bubble_tmp.gdb
